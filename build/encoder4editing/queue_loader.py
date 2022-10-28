53797a42f58ef97c2a23cd3221c893e42b19a06a
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-28 22:03:51 +0900

    :zap: Update code

--- build/encoder4editing/queue_loader.py
+++ build/encoder4editing/queue_loader.py
@@ -4,11 +4,28 @@ import subprocess
 
 
 if __name__ == "__main__":
-
+#   taskList = [
+#       {
+#           "rp_idx": 23,
+#           "ac_text": "happy",
+#       },
+#       {
+#           "rp_idx": 24,
+#           "ac_text": "happy",
+#       }
+#   ]
+  
   taskList = RequestQueue()
   taskQueue = ParseQueue(taskList)
   total = len(taskQueue)
 
+  output = []
   for cid, htext in taskQueue.items():
       command = shlex.split(f"python3 app.py --cid {cid} --htext {htext} --total {total}")
-      result = subprocess.run(command, stdout=subprocess.PIPE)
+      proc = subprocess.run(command, stdout=subprocess.PIPE)
+      stdout = proc.stdout.decode('utf-8')
+      parsedStdout = eval(out[out.find('{'):])
+      output.append(parsedStdout)
+      
+  #result = dict(Status=1, Msg="Success", Type=1, Data=data)
+  #response = dict(Result=result, flag="off")
