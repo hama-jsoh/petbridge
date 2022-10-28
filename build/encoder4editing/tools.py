653049e7782f79ceb8da2c5d7faaab43acd61ee3
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-28 16:58:17 +0900

    :zap: Add tools.py

---
+++ build/encoder4editing/tools.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def RequestQueue():
+    """
+    대기열 요청 함수
+    """
+    url = "http://devapi.petbridge.co.kr/api/remembrance/ai?done=0"
+    response = requests.get(url).json()
+    taskList = response['Data']['Result']['List']
+    return taskList
+
+def ParseQueue(tasklist):
+    """
+    대기열 파서
+    """
+    hostTexts = []
+    containerIds = []
+    for task in tasklist:
+        hostTexts.append(task['ac_text'])
+        containerIds.append(task['rp_idx'])
+    taskQueue = dict(zip(containerIds, hostTexts))
+    return taskQueue
+
+def LoadQueue():
+    """
+    불러온 대기열 파싱해서 리턴
+    """
+    taskList = RequestQueue()
+    taskQueue = ParseQueue(taskList)
+    return taskQueue
+
+def ChangeStatus(result):
+    """
+    람다에 최종 결과 & 인스턴스 종료(상태) 메시지 전송
+    """
+    lambdaUrl = "https://34ml67fwcb.execute-api.ap-northeast-2.amazonaws.com/aiStyleInstanceCon/instance"
+    res = requests.post(lambdaUrl, json=result)
+    print(res.text)
