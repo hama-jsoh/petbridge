daab805b785074a9921007c6c285f77f8707dcf9
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-28 21:03:45 +0900

    :zap: Update code

--- build/encoder4editing/queue_loader.py
+++ build/encoder4editing/queue_loader.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
 
   taskList = RequestQueue()
   taskQueue = ParseQueue(taskList)
+  total = len(taskQueue)
 
   for cid, htext in taskQueue.items():
-      command = shlex.split(f"python3 app.py --cid {cid} --htext {htext}")
+      command = shlex.split(f"python3 app.py --cid {cid} --htext {htext} --total {total}")
       result = subprocess.run(command, stdout=subprocess.PIPE)
