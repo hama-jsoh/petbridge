11740fc82fdec9f841a875a09eb72c08af7d1fef
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-29 21:48:02 +0900

    :zap: Hotfix

--- build/encoder4editing/queue_loader.py
+++ build/encoder4editing/queue_loader.py
@@ -1,31 +1,54 @@
 from tools import ParseQueue
 import shlex
 import subprocess
+from s3 import ReturnMsg
 
 
 if __name__ == "__main__":
-#   taskList = [
-#       {
-#           "rp_idx": 23,
-#           "ac_text": "happy",
-#       },
-#       {
-#           "rp_idx": 24,
-#           "ac_text": "happy",
-#       }
-#   ]
-  
-  taskList = RequestQueue()
-  taskQueue = ParseQueue(taskList)
-  total = len(taskQueue)
 
-  output = []
-  for cid, htext in taskQueue.items():
-      command = shlex.split(f"python3 app.py --cid {cid} --htext {htext} --total {total}")
-      proc = subprocess.run(command, stdout=subprocess.PIPE)
-      stdout = proc.stdout.decode('utf-8')
-      parsedStdout = eval(out[out.find('{'):])
-      output.append(parsedStdout)
-      
-  #result = dict(Status=1, Msg="Success", Type=1, Data=data)
-  #response = dict(Result=result, flag="off")
+#     DATA = [
+#         {
+#             "rp_idx": 23,
+#             "ac_text": "happy",
+#         },
+#         {
+#             "rp_idx": 24,
+#             "ac_text": "happy",
+#         }
+#     ]
+
+#     taskQueue = ParseQueue(DATA)
+
+
+    taskList = RequestQueue()
+    taskQueue = ParseQueue(taskList)
+    total = len(taskQueue)
+  
+    fail = []
+    success = []
+    for cid, htext in taskQueue.items():
+        command = shlex.split(f"python3 app.py --cid {cid} --htext {htext}")
+        proc = subprocess.run(command, stdout=subprocess.PIPE)
+
+        out = proc.stdout.decode('utf-8')
+        response = eval(out[out.find('{'):])
+        resultData = response['Result']['Data']
+
+        data_f = resultData['fail']
+        data_s = resultData['success']
+        if data_f:
+            try:
+                fail.append(data_f[0])
+            except:
+                fail.append(data_f)
+        elif data_s:
+            try:
+                success.append(data_s[0])
+            except:
+                success.append(data_s)
+
+    data = dict(total=total, fail=fail, success=success)
+    result = ReturnMsg(1, "Success", 1, data)
+    RESULT = dict(Result=result, flag='off')
+
+    ChangeStatus(RESULT)
