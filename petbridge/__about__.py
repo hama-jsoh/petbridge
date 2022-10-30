fa916e6dbf947a02412adddea069c1574885c1de
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-30 11:46:48 +0900

    :lipstick: Update project info

---
+++ petbridge/__about__.py
@@ -0,0 +1,22 @@
+__title__ = "petbridge"
+__logo__ = """\033[93m
+8888888b.          888    888888b.           d8b      888      ascii art by
+888   Y88b         888    888  "88b          Y8P      888              TAAG 
+888    888         888    888  .88P                   888                   
+888   d88P .d88b.  888888 8888888K.  888d888 888  .d88888  .d88b.   .d88b.  
+8888888P" d8P  Y8b 888    888  "Y88b 888P"   888 d88" 888 d88P"88b d8P  Y8b 
+888       88888888 888    888    888 888     888 888  888 888  888 88888888 
+888       Y8b.     Y88b.  888   d88P 888     888 Y88b 888 Y88b 888 Y8b.     
+888        "Y8888   "Y888 8888888P"  888     888  "Y88888  "Y88888  "Y8888  
+                                                               888          
+                                                          Y8b d88P          
+                                                           "Y88P"           
+\033[0m
+"""
+__description__ = (
+    "deep-learning inference software for Generative Adversarial Networks developed by HAMA"
+)
+__version__ = "0.0.4"
+__author__ = "juseokoh"
+__author_email__ = "juseok@hamagroups.io"
+__url__ = "https://www.hamagroups.io/"
